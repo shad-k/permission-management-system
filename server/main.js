55319ff90089ec293a62d278649a2ceaa0d370c8
shad-k <shadab.2005@gmail.com>
2017-04-18 13:21:03 +0530

    Update packages and other build dependency

--- server/main.js
+++ server/main.js
@@ -1,5 +1,3 @@
-import { Meteor } from 'meteor/meteor';
+import '../imports/api/roles.js';
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+import '../imports/api/messages.js';
